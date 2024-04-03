0df1cc5e3009ba1a8fe1cef9199a1eb67764544c
Cédric Brunet <brunetced42@gmail.com>
2024-04-03 16:01:47 +0200

    fix: typo in initialisation data

--- src/main/java/fr/esgi/initialisation/AjoutDonneesInitiales.java
+++ src/main/java/fr/esgi/initialisation/AjoutDonneesInitiales.java
@@ -27,11 +27,11 @@ public class AjoutDonneesInitiales implements CommandLineRunner {
     Famille famille_2 = familleRepository.save(new Famille(2L, "Famille 2", "237D57"));
 
     typeDeGraineRepository.save(new TypeDeGraine(1L, "Carotte", "description Carotte...", 13, 14, 60.0f, 5.0f, "conseils Carotte...", famille_1));
-    typeDeGraineRepository.save(new TypeDeGraine(2L, "Courgette", "description Courgette...", 22, 23, 120.0f, 75.0f, "conseils Carotte...", famille_1));
-    typeDeGraineRepository.save(new TypeDeGraine(3L, "Haricot", "description Haricot...", 22, 23, 100.0f, 15.0f, "conseils Carotte...", famille_1));
-    typeDeGraineRepository.save(new TypeDeGraine(3L, "Laitue Pommée", "description Laitue Pommée...", 13, 14, 55.0f, 25.0f, "conseils Carotte...", famille_1));
-    typeDeGraineRepository.save(new TypeDeGraine(3L, "Oignon", "description Oignon...", 14, 14, 40.0f, 8.0f, "conseils Carotte...", famille_2));
-    typeDeGraineRepository.save(new TypeDeGraine(3L, "Radis", "description Radis...", 14, 16, 45.0f, 2.5f, "conseils Carotte...", famille_2));
-    typeDeGraineRepository.save(new TypeDeGraine(3L, "Tomate", "description Tomate...", 21, 24, 90.0f, 80.0f, "conseils Carotte...", famille_2));
+    typeDeGraineRepository.save(new TypeDeGraine(2L, "Courgette", "description Courgette...", 22, 23, 120.0f, 75.0f, "conseils Courgette...", famille_1));
+    typeDeGraineRepository.save(new TypeDeGraine(3L, "Haricot", "description Haricot...", 22, 23, 100.0f, 15.0f, "conseils Haricot...", famille_1));
+    typeDeGraineRepository.save(new TypeDeGraine(3L, "Laitue Pommée", "description Laitue Pommée...", 13, 14, 55.0f, 25.0f, "conseils Laitue Pommée...", famille_1));
+    typeDeGraineRepository.save(new TypeDeGraine(3L, "Oignon", "description Oignon...", 14, 14, 40.0f, 8.0f, "conseils Oignon...", famille_2));
+    typeDeGraineRepository.save(new TypeDeGraine(3L, "Radis", "description Radis...", 14, 16, 45.0f, 2.5f, "conseils Radis...", famille_2));
+    typeDeGraineRepository.save(new TypeDeGraine(3L, "Tomate", "description Tomate...", 21, 24, 90.0f, 80.0f, "conseils Tomate...", famille_2));
   }
 }
